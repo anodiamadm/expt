5ac73b43617021e1522f4a5e891dee25c19b5fcd
Anodiam <66468713+anodiamadm@users.noreply.github.com>
2023-05-26 14:04:26 +0530

    Avatar complete

--- React MUI/react-mui-demo/src/components/MuiAvatar.tsx
+++ React MUI/react-mui-demo/src/components/MuiAvatar.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Avatar, Stack} from '@mui/material'
+import { Avatar, Stack, AvatarGroup } from '@mui/material'
 
 const MuiAvatar = () => {
     return (
@@ -9,16 +8,23 @@ const MuiAvatar = () => {
                 <Avatar sx={{bgcolor:'success.light'}}>CK</Avatar>
             </Stack>
             <Stack direction='row' spacing={1}>
-                <Avatar sx={{bgcolor:'primary.light'}}>BW</Avatar>
-                <Avatar sx={{bgcolor:'success.light'}}>CK</Avatar>
-                <Avatar 
-                    src='https://randomuser.me/api/portraits/women/79.jpg'
-                    alt='Jane Doe'
-                />
-                <Avatar 
-                    src='https://randomuser.me/api/portraits/men/61.jpg'
-                    alt='John Doe'
-                />
+                <Avatar sx={{bgcolor:'primary.light', width: 48, height: 30}}>BW</Avatar>
+                <Avatar sx={{bgcolor:'success.light', width: 48, height: 48}} variant='square'>CK</Avatar>
+                <Avatar sx={{bgcolor:'primary.light', width: 48, height: 48}} variant='rounded'>XY</Avatar>
+            </Stack>
+            <Stack direction='row' spacing={1}>
+                <AvatarGroup max={3}>
+                    <Avatar sx={{bgcolor:'primary.light'}}>BW</Avatar>
+                    <Avatar sx={{bgcolor:'success.light'}}>CK</Avatar>
+                    <Avatar 
+                        src='https://randomuser.me/api/portraits/women/79.jpg'
+                        alt='Jane Doe'
+                    />
+                    <Avatar 
+                        src='https://randomuser.me/api/portraits/men/61.jpg'
+                        alt='John Doe'
+                    />
+                </AvatarGroup>
             </Stack>
         </Stack>
     )
